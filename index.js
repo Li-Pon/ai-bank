c5c2fa9518a07a0586ba3ff84240228dc9680639
Li Pon <mirnatul@gmail.com>
2022-07-26 06:27:00 +0600

    add

---
+++ index.js
@@ -0,0 +1,39 @@
+// login button event handler
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click", function() {
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display = "none";
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
+});
+
+// deposit handler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function() {
+    const depositNumber = getInputNumber("depositAmount");
+
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+    
+    document.getElementById("depositAmount").value = "";
+});
+
+// withdraw handler
+const withdrawBtn = document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click", function () {
+    const withdrawNumber = getInputNumber("withdrawAmount");
+
+});
+
+function getInputNumber(id) {
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
+}
+
+function updateSpanText(id, depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+};
